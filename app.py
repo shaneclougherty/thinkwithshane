2e571d28617557f7a928bcf1984506939e4a7371
shaneclougherty <shaneclougherty89@gmail.com>
2026-03-22 15:18:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_title="Think With Shane",  
     page_icon="💠",                 
     layout="wide", 
-    initial_sidebar_state="expanded" # <--- FIXED: This forces the sidebar to show up immediately
+    initial_sidebar_state="collapsed"
 )
 
 # Initialize Supabase
